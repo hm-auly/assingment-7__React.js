e0c2c29162f234a24befd57fc238f414a7132986
Auly <github.git111@gmail.com>
2025-01-22 22:34:51 +0600

    hover efact added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import Header from "./Components/Header";
-import Hiro from "./Components/Hiro";
 import Container from "./Components/Container";
 
 function App() {
--- src/Components/Hiro.jsx
+++ src/Components/Hiro.jsx
@@ -10,7 +10,7 @@ function Hiro() {
                     <h5 className='text-primary'>AI SDRs &#40; aiDRs &#41;</h5>
                     <h1 className='text-4xl md:text-5xl lg:text-5xl font-semibold py-8'>More <i className='font-medium'>leads</i>,<br /> less <i className='font-medium'>people</i>.</h1>
                     <p className='text-secondar pb-7 max-w-sm'>Train an aiDR on your ICP and messaging matrix. Activate it on a patch. It will send personalized sequences to every target contact.</p>
-                    <Button color="primary" radius="full" className='text-gray-900 w-full md:w-52'>Sign Up for the Beta<i class="fa-solid fa-arrow-up-right-from-square"></i></Button>
+                    <Button color="primary" radius="full" className='text-gray-900 w-full md:w-52 hover:scale-x-105 hover:shadow-md hover:shadow-primary transition-all'>Sign Up for the Beta<i class="fa-solid fa-arrow-up-right-from-square"></i></Button>
                 </div>
             </div>
         </section>
