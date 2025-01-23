232a0797d4418a8265f67d44aaaa2f42bb008ce4
Auly <github.git111@gmail.com>
2025-01-23 13:43:38 +0600

    Buttin Hover Efect Added

--- src/Components/Embrace.jsx
+++ src/Components/Embrace.jsx
@@ -11,7 +11,7 @@ function Embrace() {
                     <h2 className='text-4xl py-5'><i>Embrace </i><span className='font-bold'>the new era of</span> <i>outbound</i></h2>
                     <p className='text-lg max-w-xl text-secondary'>Wizia lets you train, activate, and optimize aiDRs. 
                     Take your outbound to new levels of performance and efficiency.</p>
-                    <Button color="primary" radius="full" className='text-gray-900 w-full md:w-52 hover:scale-x-105 hover:shadow-md hover:shadow-primary transition-all mt-12'>Sign Up for the Beta<i class="fa-solid fa-arrow-up-right-from-square"></i></Button>
+                    <Button color="primary" radius="full" className='text-gray-900 w-full md:w-52 hover:scale-x-105 hover:shadow-md hover:bg-black hover:text-white hover:shadow-gray-500 duration-300 transition-all mt-12'>Sign Up for the Beta<i class="fa-solid fa-arrow-up-right-from-square"></i></Button>
                 </div>
             </div>
         </section>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,12 @@ function Footer() {
   return (
     <>
       <section className='bg-[#07292F] md:px-24 px-5 py-8'>
-        <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center gap-8 flex-col md:flex-row'>
             <div>
                 <img src="/Images/Logo2.svg" alt="" />
             </div>
             <div>
-                <h1 className='text-secondary text-xl'>Copyright © 2025 Wizia. All rights reserved.</h1>
+                <h1 className='text-secondary text-xl text-center md:text-start'>Copyright © 2025 Wizia. All rights reserved.</h1>
             </div>
         </div>
       </section>
