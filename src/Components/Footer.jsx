89be1d84e3b3e73fef2173955c748d6032ded972
Auly <github.git111@gmail.com>
2025-01-23 13:28:33 +0600

    Footer Text Update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      <section className='bg-[#07292F] md:px-24 px-5 py-5'>
+      <section className='bg-[#07292F] md:px-24 px-5 py-8'>
         <div className='flex justify-between items-center'>
             <div>
                 <img src="/Images/Logo2.svg" alt="" />
             </div>
             <div>
-                <h1>Hello</h1>
+                <h1 className='text-secondary text-xl'>Copyright © 2025 Wizia. All rights reserved.</h1>
             </div>
         </div>
       </section>
